6f6b0a6387f50d9ab4097efe9e842578a9b2ce3e
rila19 <rico.lautenschlaeger.2@gmail.com>
2023-10-02 13:01:34 +0200

    Update Aufgabe4.java

--- whileLoop/aufgabe4.java
+++ whileLoop/aufgabe4.java
@@ -1,2 +1,31 @@
-package whileLoop;public class aufgabe4 {
+package whileLoop;
+/*
+Aufgabe 4
+Implementieren die Funktionen ggt, die den größten gemeinsamen Teiler von 2
+ganzen Zahlen zurückgibt. Die Signatur der Funktion ist wie folgt:
+public static int ggt(int n, int m);
+Überprüfen Sie Ihre eigene Implementierung anhand von Aufrufen in einer main
+Methode.
+ */
+public class aufgabe4 {
+    public static int ggt(int n, int m){
+    int i = 1;
+    int teiler = 0;
+
+        while(i <= n || i <= m){
+           double a = n % i;
+           double b = m % i;
+            if(a == 0 && b == 0){
+               teiler = i;
+                //System.out.println("da :"+i);
+            }
+            i++;
+        }
+    return(teiler);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Der größte Teiler ist: "+ggt(42, 15));
+    }
 }
